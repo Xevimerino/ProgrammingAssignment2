508a14265b9cae2856991f47e1d790628bd86bdf
Xevi <ikit4@hotmail.com>
2014-05-24 14:01:30 +0200

    Updated explanations and corrected typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,11 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## This function creates a list of functions calculates the 
+## This function creates a list of functions that calculate the inverse of a
+##matrix and also set the variables to see if the function has its inverse stored
 
 makeCacheMatrix <- function(x = matrix()) { 
-        i <- NULL
+          i <- NULL
         ##Defines a function that when it is called it takes the variable x and equals it
         ##to the argument. Then sets i to null.
         set <- function(y) {
@@ -29,12 +30,17 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         i <- x$getsolve()
+        #If i is empty calculates the inverse, if not, gets the cached data
         if(!is.null(i)) {
                 message("getting cached data")
                 return(i)
         }
+        #Here it gets the matrix
         data <- x$get()
+        #Makes the inverse
         i <- solve(data, ...)
+        #And stores it...
         x$setsolve(i)
-        i
         ## Return a matrix that is the inverse of 'x'
+        i
+}
